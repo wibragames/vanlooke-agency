28a167a45e3609754d3d00aff2892775b6f4423b
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-10-28 09:48:53 +0200

    Finished React in Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us">
+      <p>Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry. </p>
+    </Layout>
+  )
+}
+
+export default AboutPage
