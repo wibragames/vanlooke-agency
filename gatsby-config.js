530f45de2b13192db335c8e4b33719057a3852d0
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-10-28 09:59:03 +0200

    Gatbsy Plugins finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: "Artist Agency",
+    description: "Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry.",
+    author: "@gatsbyjs",
+    siteUrl: "https://gatsbystarterdefaultsource.gatsbyjs.io/",
   },
   plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-      <Layout pageTitle="Welcome to Inghelbrecht Agency!">
+    <main>
+      <Layout pageTitle="Welcome to Van Looke Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Een Gatsby astronaut"
+        src="../images/joaquin-profile.png"
+      />
       </Layout>
+    </main>
   )
 }
 
